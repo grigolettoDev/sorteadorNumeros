b9906335bc045c00eb0514416aaee764fd7384e8
Leonardo Grigoletto <grigolettodev@gmail.com>
2025-04-03 15:37:55 -0300

    gera numeros

--- app.js
+++ app.js
@@ -6,11 +6,36 @@ function sortear(){
     console.log(quantidade);
     console.log(de);
     console.log(ate);
+    let numerosSorteados = gerandoNumeros(quantidade,de,ate);
+    console.log(numerosSorteados);
 
 }
 
 function pegandoValores(id){
     let elementoHtml = document.getElementById(id);
     let valor = elementoHtml.value;
+    valor = parseInt(valor);
     return valor;
+}
+
+function gerandoNumeros(qtdTotal,valorMinimo,valorMaximo){
+
+    let numerosGerados = [];
+    let qtdNumerosGerados = 0;
+    let numeroGerado;
+
+    while(qtdNumerosGerados<qtdTotal){
+
+        numeroGerado = Math.floor(Math.random() * (valorMaximo - valorMinimo + 1)) + valorMinimo;
+
+        if(numerosGerados.includes(numeroGerado)){
+            continue;
+        } else{
+            numerosGerados.push(numeroGerado);
+        }
+
+        qtdNumerosGerados++;
+    }
+
+    return numerosGerados;
 }
